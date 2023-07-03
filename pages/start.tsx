56b80869c8f877dfe4081d0714eb7761ea93cc55
Eytan Levy <eslevy17@gmail.com>
2023-07-02 19:02:01 -0700

    Added Start page

---
+++ pages/start.tsx
@@ -0,0 +1,9 @@
+export default function Start() {
+    return (
+        <div>
+            <p>Starting my first Next.js page</p>
+            <p>Wherein we'll use every feature Next.js has to offer.</p>
+            <p>Well...most of them. The ones I think would be good to practice.</p>
+        </div>
+    )
+}
