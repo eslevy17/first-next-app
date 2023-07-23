84500ddd56395cd03dd289b2872146dfc0c39e31
Eytan Levy <eslevy17@gmail.com>
2023-07-22 20:50:03 -0700

    Update TopBar.tsx CSS

--- app/_components/layout/TopBar.tsx
+++ app/_components/layout/TopBar.tsx
@@ -2,18 +2,26 @@ const topBarCSS: string = `
     col-span-12
     p-2
     border-b
+    border-sky-900
     flex
-    justify-center
     sticky
     top-0
     z-10
-    bg-slate-900
+    backdrop-blur
+    bg-slate-900/50
 `;
+
+const topBarTitleCSS = `
+    text-xl
+    font-extrabold
+    text-gray-300
+    ml-2
+`
 
 export function TopBar() {
     return (
         <div className={topBarCSS}>
-            <p>First Next Project</p>
+            <h1 className={topBarTitleCSS}>First Next Project</h1>
         </div>
     )
 }
