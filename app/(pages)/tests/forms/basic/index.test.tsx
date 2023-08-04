9709f1a5343e60f7538965a66636ec76ffacec8e
Eytan Levy <eslevy17@gmail.com>
2023-08-03 18:55:43 -0700

    Added form update tests for basic form test page

--- app/(pages)/tests/forms/basic/index.test.tsx
+++ app/(pages)/tests/forms/basic/index.test.tsx
@@ -1,14 +1,51 @@
-import { render, screen } from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import Page from './page'
 import '@testing-library/jest-dom'
 
 describe('Page', () => {
-    it('Finds all the inputs', () => {
+    it('Updates the text input', () => {
         render(<Page />)
-        const textInput = screen.getByRole('textbox', { name: /text/i} )
-        const numberInput = screen.getByRole('spinbutton', { name: /number/i} )
-        const date = screen.getByLabelText(/date/i)
-        const time = screen.getByLabelText(/time/i)
-        const submitButton = screen.getByRole('button', { name: /submit/i})
+        const textInput = screen.getByRole<HTMLInputElement>('textbox', { name: /text/i} )
+        const desiredText = 'add some text'
+        fireEvent.change(textInput, {target: {value: desiredText}})
+        expect(textInput.value).toBe(desiredText)
+    })
+})
+
+describe('Page', () => {
+    it('Updates the number input', () => {
+        render(<Page />)
+        const numberInput = screen.getByRole<HTMLInputElement>('spinbutton', { name: /number/i} )
+        const desiredNumber = '23'
+        fireEvent.change(numberInput, {target: {value: desiredNumber}})
+        expect(numberInput.value).toBe(desiredNumber)
+    })
+})
+
+describe('Page', () => {
+    it('Updates the date input', () => {
+        render(<Page />)
+        const dateInput = screen.getByLabelText<HTMLInputElement>(/date/i)
+        const desiredDate = '2020-05-24'
+        fireEvent.change(dateInput, {target: {value: desiredDate}})
+        expect(dateInput.value).toBe(desiredDate)
+    })
+})
+
+describe('Page', () => {
+    it('Updates the time input', () => {
+        render(<Page />)
+        const timeInput = screen.getByLabelText<HTMLInputElement>(/time/i)
+        const desiredTime = '10:30:28'
+        fireEvent.change(timeInput, {target: {value: desiredTime}})
+        expect(timeInput.value).toBe(desiredTime)
+    })
+})
+
+describe('Page', () => {
+    it('Clicks the submit button', () => {
+        render(<Page />)
+        const submitButton = screen.getByRole<HTMLButtonElement>('button', { name: /submit/i})
+        fireEvent.click(submitButton)
     })
 })
